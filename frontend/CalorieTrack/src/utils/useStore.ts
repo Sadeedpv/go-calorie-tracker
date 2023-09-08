b871004da8e55155ee15bffaf0f4ab2b896f1906
Sadeed pv <muhammedsadeed70@gmail.com>
2023-09-08 17:18:19 +0530

    Add zustand State Management

---
+++ frontend/CalorieTrack/src/utils/useStore.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand"
+import { calorieData } from "./type"
+
+interface calorieState{
+    calorieData: calorieData[],
+    totalCalories: number,
+    fetchCalorieData: () => void,
+    fetchTotalCalories: () => void
+}
+
+export const useCalorieStore = create<calorieState>()((set) => ({
+    calorieData: [],
+    totalCalories: 0,
+    fetchCalorieData: () => {
+        fetch(`${import.meta.env.VITE_PORT}/calories`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+        .then(res => res.json())
+        .then(data => {
+            set({ calorieData: data })
+        })
+    },
+    fetchTotalCalories: () => {
+        fetch(`${import.meta.env.VITE_PORT}.totalcalories`,
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        .then(res => res.json())
+        .then(data => {
+            set({ totalCalories: data.TotalCalories });
+        })
+        
+    }
+}))
